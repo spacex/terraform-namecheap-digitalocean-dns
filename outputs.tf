3a2bb46cbec32886781a8e34b594297fcbd7dcce
Michael Williams <mspacex@gmail.com>
2024-03-02 08:34:57 -0600

    tfm

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
 output "domains" {
-  value = digitalocean_domain.do_domains
+  value       = digitalocean_domain.do_domains
   description = <<-EOT
     Domain resources created (digitalocean_domain)
   EOT
 }
 
 output "dns_records" {
-  value = digitalocean_record.do_records
+  value       = digitalocean_record.do_records
   description = <<-EOT
     DNS record resources created (digitalocean_record)
   EOT
