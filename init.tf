946166c0fb7f72e6f2a508055211f0a0c0409f66
Michael Williams <mspacex@gmail.com>
2024-03-04 02:26:23 -0600

    Add known version constraints.

--- init.tf
+++ init.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
+      version = ">= 2.25.2"
     }
     namecheap = {
       source  = "namecheap/namecheap"
-      version = "2.0.2"
+      version = ">= 2.0.2"
     }
   }
 }
